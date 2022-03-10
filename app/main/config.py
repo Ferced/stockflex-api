de9b73bcfdf8b620ae7069a8adf36e66b083e90d
Joaquin Fernandez Cedeira <fcedeirajoaquin@gmail.com>
2022-03-10 11:49:02 -0300

    added .env file and adapted config.py file

--- app/main/config.py
+++ app/main/config.py
@@ -1,5 +1,9 @@
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 basedir = os.path.abspath(os.path.dirname(__file__))
 os.environ["REVERSEPROXY_ENV"] = "dev"
 
@@ -12,12 +16,12 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MONGODB_HOST = "localhost"
-    MONGODB_PORT = 27017
-    MONGODB_NAME = "reverse-proxy-dev"
-    MONGODB_USERNAME = ""
-    MONGODB_PASSWORD = ""
-    MONGODB_AUTH_SOURCE = ""
+    MONGODB_HOST = os.environ["MONGO_HOST"]
+    MONGODB_PORT = int(os.environ["MONGO_PORT"])
+    MONGODB_NAME = os.environ["MONGO_DB"]
+    MONGODB_USERNAME = os.environ["MONGO_USERNAME"]
+    MONGODB_PASSWORD = os.environ["MONGO_PASSWORD"]
+    MONGODB_AUTH_SOURCE = os.environ["MONGO_AUTH_SOURCE"]
     REDIS_HOST = "redis"
     REDIS_PORT = "6379"
 
@@ -25,24 +29,24 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    MONGODB_HOST = "localhost"
-    MONGODB_PORT = 27017
-    MONGODB_NAME = "reverse-proxy-test"
-    MONGODB_USERNAME = ""
-    MONGODB_PASSWORD = ""
-    MONGODB_AUTH_SOURCE = ""
+    MONGODB_HOST = os.environ["MONGO_HOST"]
+    MONGODB_PORT = int(os.environ["MONGO_PORT"])
+    MONGODB_NAME = os.environ["MONGO_DB"]
+    MONGODB_USERNAME = os.environ["MONGO_USERNAME"]
+    MONGODB_PASSWORD = os.environ["MONGO_PASSWORD"]
+    MONGODB_AUTH_SOURCE = os.environ["MONGO_AUTH_SOURCE"]
     REDIS_HOST = "localhost"
     REDIS_PORT = "6379"
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
 class ProductionConfig(Config):
-    MONGODB_HOST = "mongodb"
-    MONGODB_PORT = 27017
-    MONGODB_NAME = "reverse-proxy-prod"
-    MONGODB_USERNAME = ""
-    MONGODB_PASSWORD = ""
-    MONGODB_AUTH_SOURCE = ""
+    MONGODB_HOST = os.environ["MONGO_HOST"]
+    MONGODB_PORT = int(os.environ["MONGO_PORT"])
+    MONGODB_NAME = os.environ["MONGO_DB"]
+    MONGODB_USERNAME = os.environ["MONGO_USERNAME"]
+    MONGODB_PASSWORD = os.environ["MONGO_PASSWORD"]
+    MONGODB_AUTH_SOURCE = os.environ["MONGO_AUTH_SOURCE"]
     REDIS_HOST = "redis"
     REDIS_PORT = "6379"
     DEBUG = False
